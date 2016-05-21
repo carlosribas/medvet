6dc558e58a89b3cfa0c37a3e4d098ceee1709426
Carlos Eduardo Ribas <caduribas@gmail.com>
2016-05-20 22:28:50 -0300

    PEP8 corrections

--- animal/views.py
+++ animal/views.py
@@ -1,5 +1,5 @@
 import json as simplejson
-from animal.models import Breed, Specie
+from animal.models import Specie
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 
