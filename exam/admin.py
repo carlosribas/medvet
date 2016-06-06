3923edffcb3252b3d3cec544544435b50858452c
Carlos Eduardo Ribas <caduribas@gmail.com>
2016-06-05 22:34:10 -0300

    New types of exam added

--- exam/admin.py
+++ exam/admin.py
@@ -2,5 +2,15 @@ from exam.models import *
 from django.contrib import admin
 
 
-admin.site.register(Parasitological)
 admin.site.register(Exam)
+admin.site.register(Parasitological)
+admin.site.register(Hematology)
+admin.site.register(RenalProfile)
+admin.site.register(HepaticProfile)
+admin.site.register(Proteins)
+admin.site.register(Endocrinology)
+admin.site.register(Electrolytes)
+admin.site.register(Microbiology)
+admin.site.register(Anatomopathology)
+admin.site.register(Image)
+
--- exam/models.py
+++ exam/models.py
@@ -7,20 +7,77 @@ from django.utils.translation import ugettext_lazy as _
 class ExamType(models.Model):
     name = models.CharField(_('Name'), max_length=255)
 
+    # Returns the name
+    def __unicode__(self):
+        return u'%s' % self.name
+
     class Meta:
         abstract = True
 
 
 class Parasitological(ExamType):
-    # Returns the name
-    def __unicode__(self):
-        return u'%s' % self.name
+    class Meta:
+        verbose_name = _('Parasitological')
+
+
+class Hematology(ExamType):
+    class Meta:
+        verbose_name = _('Hematology')
+
+
+class RenalProfile(ExamType):
+    class Meta:
+        verbose_name = _('Renal profile')
+
+
+class HepaticProfile(ExamType):
+    class Meta:
+        verbose_name = _('Hepatic profile')
+
+
+class Proteins(ExamType):
+    class Meta:
+        verbose_name = _('Proteins')
+
+
+class Endocrinology(ExamType):
+    class Meta:
+        verbose_name = _('Endocrinology')
+
+
+class Electrolytes(ExamType):
+    class Meta:
+        verbose_name = _('Electrolytes')
+
+
+class Microbiology(ExamType):
+    class Meta:
+        verbose_name = _('Microbiology')
+
+
+class Anatomopathology(ExamType):
+    class Meta:
+        verbose_name = _('Anatomopathology')
+
+
+class Image(ExamType):
+    class Meta:
+        verbose_name = _('Image')
 
 
 class Exam(models.Model):
     animal = models.ForeignKey(Animal, verbose_name=_("Animal's Name"))
     date = models.DateField(_('Date'), default=datetime.date.today)
-    type_parasitological = models.ForeignKey(Parasitological)
+    parasitological_exam = models.ForeignKey(Parasitological)
+    hematology_exam = models.ForeignKey(Hematology)
+    renal_profile_exam = models.ForeignKey(RenalProfile)
+    hepatic_profile_exam = models.ForeignKey(HepaticProfile)
+    proteins_exam = models.ForeignKey(Proteins)
+    endocrinology_exam = models.ForeignKey(Endocrinology)
+    electrolytes_exam = models.ForeignKey(Electrolytes)
+    microbiology_exam = models.ForeignKey(Microbiology)
+    anatomopathology_exam = models.ForeignKey(Anatomopathology)
+    image_exam = models.ForeignKey(Image)
 
     # Returns the date
     def __unicode__(self):
