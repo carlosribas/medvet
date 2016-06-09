a55236ad161b19a3d54e3bf49cf43ac5ccc18c07
Carlos Eduardo Ribas <caduribas@gmail.com>
2016-06-09 11:29:04 -0300

    Add javascript to filter animal

--- static/js/physical_examination.js
+++ static/js/physical_examination.js
@@ -225,24 +225,3 @@ $(document).ready(function () {
     });
     if (id_central_and_peripheral_nervous_system_0.checked){$("#id_central_and_peripheral_nervous_system_note").parents('.control-group').hide();}
 });
-
-function ajax_filter_animal_name(owner_id)
-{
-   $("#id_animal").html('<option value="">Carregando...</option>');
-   $.ajax({
-       type: "GET",
-       url: "/physical_examination/select_animal",
-       dataType: "json",
-       data: {'owner':owner_id},
-       success: function(retorno) {
-           $("#id_animal").empty();
-           $("#id_animal").append('<option value="">--------</option>');
-           $.each(retorno, function(i, item){
-               $("#id_animal").append('<option value="'+item.pk+'">'+item.valor+'</option>');
-           });
-       },
-       error: function(erro) {
-           alert('Erro: Sem retorno de requisição.');
-       }
-   });
-}
