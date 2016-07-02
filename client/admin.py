8498135a0451c58ba0426b4104a394ae91af299d
Carlos Eduardo Ribas <caduribas@gmail.com>
2016-07-02 11:14:18 -0300

    a table was created to store contacts

--- client/admin.py
+++ client/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 from forms import ClientAdminForm
-from models import Client
+from models import Client, ClientContact
 from django.utils.translation import ugettext_lazy as _
+
+
+class ClientContactInline(admin.TabularInline):
+    model = ClientContact
+    extra = 1
 
 
 class ClientAdmin(admin.ModelAdmin):
 
     fieldsets = [
         (None, {
-            'fields': ['name', 'email', 'phone', 'cellphone', 'note', 'another_responsible',
-                       'another_responsible_phone']
+            'fields': ['name', 'email', 'phone', 'cellphone', 'note']
         }),
         (_('Address'), {
             'fields': ['zipcode', 'street', 'number', 'street_complement', 'district', 'city', 'state'],
@@ -24,6 +28,7 @@ class ClientAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'phone', 'cellphone')
     list_display_links = ('name', )
     search_fields = ['name', 'email', 'note', 'cpf']
+    inlines = (ClientContactInline,)
     form = ClientAdminForm
 
 admin.site.register(Client, ClientAdmin)
--- client/models.py
+++ client/models.py
@@ -9,37 +9,32 @@ def validate_cpf(value):
     Checks if the CPF is valid
 
     """
-
     validation = CPF(value)
     if not validation.isValid():
         raise ValidationError(_('%s is not valid CPF') % value)
 
 
 class Client(models.Model):
-
     """
     An instance of this class represents a client.
 
     '__unicode__'		Returns the name.
     'class Meta'		Sets the description model (singular and plural) and define ordering of data by name.
     """
-
     name = models.CharField(_('Name'), max_length=255)
-    rg = models.CharField(_('RG'), max_length=12, blank=True, null=True)
-    cpf = models.CharField(_('CPF'), blank=True, null=True, max_length=15, validators=[validate_cpf])
+    rg = models.CharField(_('RG'), max_length=12, blank=True)
+    cpf = models.CharField(_('CPF'), blank=True, max_length=15, validators=[validate_cpf])
     email = models.EmailField(_('E-mail'), blank=True, null=True)
-    phone = models.CharField(_('Phone'), max_length=15, blank=True, null=True)
-    cellphone = models.CharField(_('Cell Phone'), max_length=15, blank=True, null=True)
-    zipcode = models.CharField(_('Zip Code'), max_length=9, blank=True, null=True)
-    street = models.CharField(_('Address'), max_length=255, blank=True, null=True)
-    street_complement = models.CharField(_('Complement'), max_length=255, blank=True, null=True)
+    phone = models.CharField(_('Phone'), max_length=15, blank=True)
+    cellphone = models.CharField(_('Cell Phone'), max_length=15, blank=True)
+    zipcode = models.CharField(_('Zip Code'), max_length=9, blank=True)
+    street = models.CharField(_('Address'), max_length=255, blank=True)
+    street_complement = models.CharField(_('Complement'), max_length=255, blank=True)
     number = models.PositiveIntegerField(_('Number'), blank=True, null=True)
-    district = models.CharField(_('District'), max_length=255, blank=True, null=True)
-    city = models.CharField(_('City'), max_length=255, blank=True, null=True)
-    state = models.CharField(_('State'), max_length=255, blank=True, null=True)
-    note = models.CharField(_('Note'), max_length=255, blank=True, null=True)
-    another_responsible = models.CharField(_('Another Responsible'), max_length=255, blank=True, null=True)
-    another_responsible_phone = models.CharField(_("Another Responsible's Phone"), max_length=15, blank=True, null=True)
+    district = models.CharField(_('District'), max_length=255, blank=True)
+    city = models.CharField(_('City'), max_length=255, blank=True)
+    state = models.CharField(_('State'), max_length=255, blank=True)
+    note = models.CharField(_('Note'), max_length=255, blank=True)
 
     # Returns the name 
     def __unicode__(self):
@@ -50,3 +45,19 @@ class Client(models.Model):
         verbose_name = _('Client')
         verbose_name_plural = _('Clients')
         ordering = ('name', )
+
+
+class ClientContact(models.Model):
+    """
+    An instance of this class represents a contact of client.
+
+    'class Meta'		Sets the description model (singular and plural) and define ordering of data by name.
+    """
+    client_name = models.ForeignKey(Client)
+    name = models.CharField(_('Name'), max_length=255, blank=True)
+    phone = models.CharField(_("Phone"), max_length=15, blank=True)
+    cellphone = models.CharField(_('Cell Phone'), max_length=15, blank=True)
+
+    class Meta:
+        verbose_name = _('Client contact')
+        verbose_name_plural = _('Client contacts')
