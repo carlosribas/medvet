86bd7ec5af7cfc3497313df6f77feb75c28e9aa8
Carlos Eduardo Ribas <caduribas@gmail.com>
2016-07-25 18:42:53 -0300

    Get number of clients and animal

--- medvet/views.py
+++ medvet/views.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from client.models import Client
+from animal.models import Animal
+
 
 @login_required
 def index(request):
-    return render(request, 'index.html')
+    clients = Client.objects.count()
+    animals = Animal.objects.count()
+    context = {'clients': clients, 'animals': animals}
+    return render(request, 'index.html', context)
