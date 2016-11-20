3885caee702e27504472044e5569244f00597fa9
Carlos Eduardo Ribas <caduribas@gmail.com>
2016-11-20 15:36:09 -0200

    Physical Examination model updated

--- physical_examination/forms.py
+++ physical_examination/forms.py
@@ -1,97 +1,8 @@
-from client.models import Client
-from physical_examination.models import Examination
 from django import forms
-from django.forms import DateInput, RadioSelect, Select, Textarea, TextInput
+from django.forms import DateInput, Select, Textarea, TextInput
 from django.utils.translation import ugettext_lazy as _
 
-
-BODY_CONDITION = (
-    ('0', _('1 - Very thin')),
-    ('1', _('2 - Underweight')),
-    ('2', _('3 - Ideal weight')),
-    ('3', _('4 - Overweight')),
-    ('4', _('5 - Obese')),
-)
-
-DEFAULT_ANSWER = (
-    ('False', _('Normal')),
-    ('True', _('Abnormal')),
-)
-
-ATTITUDE_ANSWER = (
-    ('0', _('Bright and Alert')),
-    ('1', _('Quiet but Alert')),
-    ('2', _('Lethargic')),
-    ('3', _('Dull')),
-    ('4', _('Hyperactive')),
-    ('5', _('Other')),
-)
-
-FACE_ANSWER = (
-    ('0', _('Normal')),
-    ('1', _('Head tilt')),
-    ('2', _('Abnormal')),
-)
-
-DEHYDRATED_ANSWER = (
-    ('0', _('No dehydration')),
-    ('1', _('Less than 5% - Reports loss of liquids')),
-    ('2', _('5 to 6% - Mild')),
-    ('3', _('7 to 9% - Moderate')),
-    ('4', _('10 to 12% - Severe')),
-    ('5', _('12 to 15% - Hypovolemic shock')),
-)
-
-MUCOUS_ANSWER = (
-    ('0', _('Pink')),
-    ('1', _('Blue')),
-    ('2', _('Pale')),
-    ('3', _('Icteric')),
-    ('4', _('Congested')),
-)
-
-
-class ExaminationAdminForm(forms.ModelForm):
-
-    owner = forms.ModelChoiceField(Client.objects.all(), label=_('Owner'),
-                                   widget=Select(attrs={'onchange': 'ajax_filter_animal_name(this.value);'}))
-
-    class Meta:
-        model = Examination
-
-        fields = ['pulse', 'body_condition', 'attitude', 'face', 'eye', 'ear', 'nose', 'hydration', 'mouth',
-                  'mucous_membrane', 'superficial_lymph_nodes', 'palpable_thyroid', 'pulmonary_auscultation',
-                  'cardiac_auscultation', 'abdominal_palpation', 'coat', 'skin', 'musculoskeletal_system',
-                  'central_and_peripheral_nervous_system']
-
-        widgets = {
-            'pulse': RadioSelect(choices=DEFAULT_ANSWER),
-            'body_condition': RadioSelect(choices=BODY_CONDITION),
-            'attitude': RadioSelect(choices=ATTITUDE_ANSWER),
-            'face': RadioSelect(choices=FACE_ANSWER),
-            'eye': RadioSelect(choices=DEFAULT_ANSWER),
-            'ear': RadioSelect(choices=DEFAULT_ANSWER),
-            'nose': RadioSelect(choices=DEFAULT_ANSWER),
-            'hydration': RadioSelect(choices=DEHYDRATED_ANSWER),
-            'mouth': RadioSelect(choices=DEFAULT_ANSWER),
-            'mucous_membrane': RadioSelect(choices=MUCOUS_ANSWER),
-            'superficial_lymph_nodes': RadioSelect(choices=DEFAULT_ANSWER),
-            'palpable_thyroid': RadioSelect(choices=DEFAULT_ANSWER),
-            'pulmonary_auscultation': RadioSelect(choices=DEFAULT_ANSWER),
-            'cardiac_auscultation': RadioSelect(choices=DEFAULT_ANSWER),
-            'abdominal_palpation': RadioSelect(choices=DEFAULT_ANSWER),
-            'coat': RadioSelect(choices=DEFAULT_ANSWER),
-            'skin': RadioSelect(choices=DEFAULT_ANSWER),
-            'musculoskeletal_system': RadioSelect(choices=DEFAULT_ANSWER),
-            'central_and_peripheral_nervous_system': RadioSelect(choices=DEFAULT_ANSWER),
-        }
-
-    class Media:
-        js = ('physical_examination/js/physical_examination.js',
-              'physical_examination/js/filter_animal_name.js')
-        css = {
-            'all': ('physical_examination/css/customization.css',)
-        }
+from models import Examination
 
 
 class ExaminationForm(forms.ModelForm):
@@ -102,45 +13,46 @@ class ExaminationForm(forms.ModelForm):
         fields = '__all__'
 
         widgets = {
-            'date': DateInput(attrs={'class': 'form-control datepicker'},),
+            'date': DateInput(attrs={'class': 'form-control datepicker', 'required': "",
+                                     'data-error': _('This field must be filled.')},),
             'temperature': TextInput(attrs={'class': 'form-control'}),
-            'pulse': RadioSelect(choices=DEFAULT_ANSWER),
+            'pulse': Select(attrs={'class': 'form-control'}),
             'pulse_note': TextInput(attrs={'class': 'form-control'}),
             'respiratory_rate': TextInput(attrs={'class': 'form-control'}),
             'heart_rate': TextInput(attrs={'class': 'form-control'}),
             'bodyweight': TextInput(attrs={'class': 'form-control'}),
-            'body_condition': Select(attrs={'class': 'form-control'}, choices=BODY_CONDITION),
-            'attitude': Select(attrs={'class': 'form-control'}, choices=ATTITUDE_ANSWER),
+            'body_condition': Select(attrs={'class': 'form-control'}),
+            'attitude': Select(attrs={'class': 'form-control'}),
             'attitude_note': TextInput(attrs={'class': 'form-control'}),
-            'face': RadioSelect(choices=FACE_ANSWER),
+            'face': Select(attrs={'class': 'form-control'}),
             'face_note': TextInput(attrs={'class': 'form-control'}),
-            'eye': RadioSelect(choices=DEFAULT_ANSWER),
+            'eye': Select(attrs={'class': 'form-control'}),
             'eye_note': TextInput(attrs={'class': 'form-control'}),
-            'ear': RadioSelect(choices=DEFAULT_ANSWER),
+            'ear': Select(attrs={'class': 'form-control'}),
             'ear_note': TextInput(attrs={'class': 'form-control'}),
-            'nose': RadioSelect(choices=DEFAULT_ANSWER),
+            'nose': Select(attrs={'class': 'form-control'}),
             'nose_note': TextInput(attrs={'class': 'form-control'}),
-            'hydration': Select(attrs={'class': 'form-control'}, choices=DEHYDRATED_ANSWER),
-            'mouth': RadioSelect(choices=DEFAULT_ANSWER),
+            'hydration': Select(attrs={'class': 'form-control'}),
+            'mouth': Select(attrs={'class': 'form-control'}),
             'mouth_note': TextInput(attrs={'class': 'form-control'}),
-            'mucous_membrane': Select(attrs={'class': 'form-control'}, choices=MUCOUS_ANSWER),
+            'mucous_membrane': Select(attrs={'class': 'form-control'}),
             'capillary_refill_time': TextInput(attrs={'class': 'form-control'}),
-            'superficial_lymph_nodes': RadioSelect(choices=DEFAULT_ANSWER),
+            'superficial_lymph_nodes': Select(attrs={'class': 'form-control'}),
             'superficial_lymph_nodes_note': TextInput(attrs={'class': 'form-control'}),
-            'palpable_thyroid': RadioSelect(choices=DEFAULT_ANSWER),
+            'palpable_thyroid': Select(attrs={'class': 'form-control'}),
             'palpable_thyroid_note': TextInput(attrs={'class': 'form-control'}),
-            'pulmonary_auscultation': RadioSelect(choices=DEFAULT_ANSWER),
+            'pulmonary_auscultation': Select(attrs={'class': 'form-control'}),
             'pulmonary_auscultation_note': TextInput(attrs={'class': 'form-control'}),
-            'cardiac_auscultation': RadioSelect(choices=DEFAULT_ANSWER),
+            'cardiac_auscultation': Select(attrs={'class': 'form-control'}),
             'cardiac_auscultation_note': TextInput(attrs={'class': 'form-control'}),
-            'abdominal_palpation': RadioSelect(choices=DEFAULT_ANSWER),
+            'abdominal_palpation': Select(attrs={'class': 'form-control'}),
             'abdominal_palpation_note': TextInput(attrs={'class': 'form-control'}),
-            'coat': RadioSelect(choices=DEFAULT_ANSWER),
+            'coat': Select(attrs={'class': 'form-control'}),
             'coat_note': TextInput(attrs={'class': 'form-control'}),
-            'skin': RadioSelect(choices=DEFAULT_ANSWER),
+            'skin': Select(attrs={'class': 'form-control'}),
             'skin_note': TextInput(attrs={'class': 'form-control'}),
-            'musculoskeletal_system': RadioSelect(choices=DEFAULT_ANSWER),
+            'musculoskeletal_system': Select(attrs={'class': 'form-control'}),
             'musculoskeletal_system_note': TextInput(attrs={'class': 'form-control'}),
-            'central_and_peripheral_nervous_system': RadioSelect(choices=DEFAULT_ANSWER),
+            'central_and_peripheral_nervous_system': Select(attrs={'class': 'form-control'}),
             'additional_findings': Textarea(attrs={'class': 'form-control', 'rows': '4'}),
         }
--- physical_examination/models.py
+++ physical_examination/models.py
@@ -1,8 +1,55 @@
 import datetime
-from animal.models import Animal
-from client.models import Client
+
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+
+from animal.models import Animal
+from client.models import Client
+
+ATTITUDE_ANSWER = (
+    ('bright_and_alert', _('Bright and Alert')),
+    ('quiet_but_alert', _('Quiet but Alert')),
+    ('lethargic', _('Lethargic')),
+    ('dull', _('Dull')),
+    ('hyperactive', _('Hyperactive')),
+    ('other', _('Other')),
+)
+
+BODY_CONDITION = (
+    ('0', _('1 - Very thin')),
+    ('1', _('2 - Underweight')),
+    ('2', _('3 - Ideal weight')),
+    ('3', _('4 - Overweight')),
+    ('4', _('5 - Obese')),
+)
+
+DEFAULT_ANSWER = (
+    ('normal', _('Normal')),
+    ('abnormal', _('Abnormal')),
+)
+
+DEHYDRATED_ANSWER = (
+    ('no_dehydration', _('No dehydration')),
+    ('reports_loss_of_liquids', _('Less than 5% - Reports loss of liquids')),
+    ('mild', _('5 to 6% - Mild')),
+    ('moderate', _('7 to 9% - Moderate')),
+    ('severe', _('10 to 12% - Severe')),
+    ('hypovolemic_shock', _('12 to 15% - Hypovolemic shock')),
+)
+
+FACE_ANSWER = (
+    ('normal', _('Normal')),
+    ('head_tilt', _('Head tilt')),
+    ('abnormal', _('Abnormal')),
+)
+
+MUCOUS_ANSWER = (
+    ('pink', _('Pink')),
+    ('blue', _('Blue')),
+    ('pale', _('Pale')),
+    ('icteric', _('Icteric')),
+    ('congested', _('Congested')),
+)
 
 
 class Examination(models.Model):
@@ -15,48 +62,52 @@ class Examination(models.Model):
 
     animal = models.ForeignKey(Animal, verbose_name=_("Animal's Name"))
     date = models.DateField(_('Date'), default=datetime.date.today)
-    temperature = models.CharField(_('Temperature'), max_length=3, blank=True, null=True)
-    pulse = models.NullBooleanField(_('Pulse'), blank=True, null=True)
-    pulse_note = models.CharField(_('Note'), max_length=100, blank=True, null=True)
-    respiratory_rate = models.CharField(_('Respiratory Rate'), max_length=10, blank=True, null=True)
-    bodyweight = models.CharField(_('Bodyweight'), max_length=10, blank=True, null=True)
-    body_condition = models.CharField(_('Body Condition Score'), max_length=1, blank=True, null=True)
-    attitude = models.CharField(_('Attitude'), max_length=1, blank=True, null=True)
-    attitude_note = models.CharField(_('Note'), max_length=100, blank=True, null=True)
-    face = models.CharField(_('Face'), max_length=1, blank=True, null=True)
-    face_note = models.TextField(_('Note'), blank=True, null=True)
-    eye = models.NullBooleanField(_('Eyes'), blank=True, null=True)
-    eye_note = models.TextField(_('Note'), blank=True, null=True)
-    ear = models.NullBooleanField(_('Ears'), blank=True, null=True)
-    ear_note = models.TextField(_('Note'), blank=True, null=True)
-    nose = models.NullBooleanField(_('Nose'), blank=True, null=True)
-    nose_note = models.TextField(_('Note'), blank=True, null=True)
-    hydration = models.CharField(_('Skin Tenting'), max_length=1, blank=True, null=True)
-    mouth = models.NullBooleanField(_('Mouth'), blank=True, null=True)
-    mouth_note = models.TextField(_('Note'), blank=True, null=True)
-    mucous_membrane = models.CharField(_('Mucous Membrane'), max_length=1, blank=True, null=True)
-    capillary_refill_time = models.CharField(_('Capillary Refill Time'), max_length=3, blank=True, null=True)
-    superficial_lymph_nodes = models.NullBooleanField(_('Superficial Lymph Nodes'), blank=True, null=True)
-    superficial_lymph_nodes_note = models.CharField(_('Note'), max_length=100, blank=True, null=True)
-    palpable_thyroid = models.NullBooleanField(_('Palpable Thyroid'), blank=True, null=True)
-    palpable_thyroid_note = models.CharField(_('Note'), max_length=100, blank=True, null=True)
-    pulmonary_auscultation = models.NullBooleanField(_('Pulmonary Auscultation'), blank=True, null=True)
-    pulmonary_auscultation_note = models.CharField(_('Note'), max_length=100, blank=True, null=True)
-    cardiac_auscultation = models.NullBooleanField(_('Cardiac Auscultation'), blank=True, null=True)
-    cardiac_auscultation_note = models.CharField(_('Note'), max_length=100, blank=True, null=True)
-    heart_rate = models.CharField(_('Heart Rate'), max_length=10, blank=True, null=True)
-    abdominal_palpation = models.NullBooleanField(_('Abdominal Palpation'), blank=True, null=True)
-    abdominal_palpation_note = models.CharField(_('Note'), max_length=100, blank=True, null=True)
-    coat = models.NullBooleanField(_('Coat'), blank=True, null=True)
-    coat_note = models.CharField(_('Note'), max_length=100, blank=True, null=True)
-    skin = models.NullBooleanField(_('Skin'), blank=True, null=True)
-    skin_note = models.CharField(_('Note'), max_length=100, blank=True, null=True)
-    musculoskeletal_system = models.NullBooleanField(_('Musculoskeletal System'), blank=True, null=True)
-    musculoskeletal_system_note = models.CharField(_('Note'), max_length=100, blank=True, null=True)
-    central_and_peripheral_nervous_system = models.NullBooleanField(_('Central and Peripheral Nervous Systems'),
-                                                                    blank=True, null=True)
-    central_and_peripheral_nervous_system_note = models.CharField(_('Note'), max_length=100, blank=True, null=True)
-    additional_findings = models.TextField(_('Note'), blank=True, null=True)
+    temperature = models.CharField(_('Temperature'), max_length=3, blank=True)
+    pulse = models.CharField(_('Pulse'), max_length=10, choices=DEFAULT_ANSWER, blank=True)
+    pulse_note = models.CharField(_('Note'), max_length=255, blank=True)
+    respiratory_rate = models.CharField(_('Respiratory Rate'), max_length=10, blank=True)
+    bodyweight = models.CharField(_('Bodyweight'), max_length=10, blank=True)
+    body_condition = models.CharField(_('Body Condition Score'), max_length=1, choices=BODY_CONDITION, blank=True)
+    attitude = models.CharField(_('Attitude'), max_length=20, choices=ATTITUDE_ANSWER, blank=True)
+    attitude_note = models.CharField(_('Note'), max_length=255, blank=True)
+    face = models.CharField(_('Face'), max_length=10, choices=FACE_ANSWER, blank=True)
+    face_note = models.CharField(_('Note'), max_length=255, blank=True)
+    eye = models.CharField(_('Eye'), max_length=10, choices=DEFAULT_ANSWER, blank=True)
+    eye_note = models.CharField(_('Note'), max_length=255, blank=True)
+    ear = models.CharField(_('Ear'), max_length=10, choices=DEFAULT_ANSWER, blank=True)
+    ear_note = models.CharField(_('Note'), max_length=255, blank=True)
+    nose = models.CharField(_('Nose'), max_length=10, choices=DEFAULT_ANSWER, blank=True)
+    nose_note = models.CharField(_('Note'), max_length=255, blank=True)
+    hydration = models.CharField(_('Skin Tenting'), max_length=25, choices=DEHYDRATED_ANSWER, blank=True)
+    mouth = models.CharField(_('Mouth'), max_length=10, choices=DEFAULT_ANSWER, blank=True)
+    mouth_note = models.CharField(_('Note'), max_length=255, blank=True)
+    mucous_membrane = models.CharField(_('Mucous Membrane'), max_length=10, choices=MUCOUS_ANSWER, blank=True)
+    capillary_refill_time = models.CharField(_('Capillary Refill Time'), max_length=3, blank=True)
+    superficial_lymph_nodes = models.CharField(_('Superficial Lymph Nodes'), max_length=10, choices=DEFAULT_ANSWER,
+                                               blank=True)
+    superficial_lymph_nodes_note = models.CharField(_('Note'), max_length=100, blank=True)
+    palpable_thyroid = models.CharField(_('Palpable Thyroid'), max_length=10, choices=DEFAULT_ANSWER, blank=True)
+    palpable_thyroid_note = models.CharField(_('Note'), max_length=100, blank=True)
+    pulmonary_auscultation = models.CharField(_('Pulmonary Auscultation'), max_length=10, choices=DEFAULT_ANSWER,
+                                              blank=True)
+    pulmonary_auscultation_note = models.CharField(_('Note'), max_length=100, blank=True)
+    cardiac_auscultation = models.CharField(_('Cardiac Auscultation'), max_length=10, choices=DEFAULT_ANSWER,
+                                            blank=True)
+    cardiac_auscultation_note = models.CharField(_('Note'), max_length=100, blank=True)
+    heart_rate = models.CharField(_('Heart Rate'), max_length=10, blank=True)
+    abdominal_palpation = models.CharField(_('Abdominal Palpation'), max_length=10, choices=DEFAULT_ANSWER, blank=True)
+    abdominal_palpation_note = models.CharField(_('Note'), max_length=100, blank=True)
+    coat = models.CharField(_('Coat'), max_length=10, choices=DEFAULT_ANSWER, blank=True)
+    coat_note = models.CharField(_('Note'), max_length=100, blank=True)
+    skin = models.CharField(_('Skin'), max_length=10, choices=DEFAULT_ANSWER, blank=True)
+    skin_note = models.CharField(_('Note'), max_length=100, blank=True)
+    musculoskeletal_system = models.CharField(_('Musculoskeletal System'), max_length=10, choices=DEFAULT_ANSWER,
+                                              blank=True)
+    musculoskeletal_system_note = models.CharField(_('Note'), max_length=100, blank=True)
+    central_and_peripheral_nervous_system = models.CharField(_('Central and Peripheral Nervous Systems'), max_length=10,
+                                                             choices=DEFAULT_ANSWER, blank=True)
+    central_and_peripheral_nervous_system_note = models.CharField(_('Note'), max_length=100, blank=True)
+    additional_findings = models.TextField(_('Note'), blank=True)
 
     # Returns the date 
     def __unicode__(self):
