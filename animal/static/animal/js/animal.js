bf1854efc386d77075d3b510a23e9dfae8807323
Carlos Eduardo Ribas <caduribas@gmail.com>
2018-04-28 17:59:42 -0300

    Add button to delete an animal

--- animal/static/animal/js/animal.js
+++ animal/static/animal/js/animal.js
@@ -44,3 +44,12 @@ function ajax_filter_specie_breed(specie_id)
        }
    });
 }
+
+function disableRemoveButton() {
+    $('#remove_button').prop('disabled', true);
+}
+
+function showDialogAndEnableRemoveButton() {
+    $('#remove_button').prop('disabled', false);
+    $('#modalRemove').modal('show');
+}
