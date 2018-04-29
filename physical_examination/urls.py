e642380cee02b1ec1f2e2bdebacf8e7f67a600fd
Carlos Eduardo Ribas <caduribas@gmail.com>
2018-04-28 23:31:05 -0300

    First attempt to view physical examination

--- physical_examination/urls.py
+++ physical_examination/urls.py
@@ -4,8 +4,8 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'select_animal$', views.select_animal, name='select_animal'),
-    url(r'^new/(?P<animal_id>\d+)/$', views.new_physical_examination, name='new_physical_examination'),
+    url(r'^new/(?P<animal_id>\d+)/$', views.physical_examination_new, name='new_physical_examination'),
     url(r'^list/(?P<animal_id>\d+)/$', views.physical_examination_list, name='physical_examination_list'),
-    # url(r'^view/(?P<animal_id>\d+)/$', views.animal_view, name='animal_view'),
+    url(r'^view/(?P<physical_examination_id>\d+)/$', views.physical_examination_view, name='physical_examination_view'),
     # url(r'^edit/(?P<animal_id>\d+)/$', views.animal_update, name='animal_edit'),
 ]
--- physical_examination/views.py
+++ physical_examination/views.py
@@ -3,6 +3,7 @@ import json as simplejson
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
+from django.db.models.deletion import ProtectedError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.utils.translation import ugettext as _
@@ -28,7 +29,7 @@ def select_animal(request):
 
 
 @login_required
-def new_physical_examination(request, animal_id, template_name="animal/animal_tabs.html"):
+def physical_examination_new(request, animal_id, template_name="animal/animal_tabs.html"):
     animal = get_object_or_404(Animal, pk=animal_id)
     physical_examination_form = ExaminationForm(request.POST or None)
 
@@ -72,34 +73,35 @@ def physical_examination_list(request, animal_id, template_name="animal/animal_t
     return render(request, template_name, context)
 
 
-# @login_required
-# def animal_view(request, animal_id, template_name="animal/animal_tabs.html"):
-#     animal = get_object_or_404(Animal, pk=animal_id)
-#     animal_form = AddAnimalForm(request.POST or None, instance=animal)
-#
-#     for field in animal_form.fields:
-#         animal_form.fields[field].widget.attrs['disabled'] = True
-#
-#     if request.method == "POST":
-#         if request.POST['action'] == "remove":
-#
-#             try:
-#                 animal.delete()
-#                 messages.success(request, _('Animal removed successfully.'))
-#                 return redirect('index')
-#             except ProtectedError:
-#                 messages.error(request, _("Error trying to delete animal."))
-#                 redirect_url = reverse("animal_view", args=(animal_id,))
-#                 return HttpResponseRedirect(redirect_url)
-#
-#     context = {"can_change": True,
-#                "animal": animal,
-#                "animal_form": animal_form,
-#                "tab": "1"}
-#
-#     return render(request, template_name, context)
-#
-#
+@login_required
+def physical_examination_view(request, physical_examination_id, template_name="animal/animal_tabs.html"):
+    physical_examination = get_object_or_404(Examination, pk=physical_examination_id)
+    physical_examination_form = ExaminationForm(request.POST or None)
+
+    for field in physical_examination_form.fields:
+        physical_examination_form.fields[field].widget.attrs['disabled'] = True
+
+    if request.method == "POST":
+        if request.POST['action'] == "remove":
+
+            try:
+                physical_examination.delete()
+                messages.success(request, _('Physical examination removed successfully.'))
+                redirect_url = reverse("physical_examination_list", args=(physical_examination.animal_id,))
+                return HttpResponseRedirect(redirect_url)
+            except ProtectedError:
+                messages.error(request, _("Error trying to delete physical examination."))
+                redirect_url = reverse("physical_examination_list", args=(physical_examination.animal_id,))
+                return HttpResponseRedirect(redirect_url)
+
+    context = {"can_change": True,
+               # "animal": animal,
+               "physical_examination_form": physical_examination_form,
+               "tab": "2"}
+
+    return render(request, template_name, context)
+
+
 # @login_required
 # def animal_update(request, animal_id, template_name="animal/animal_tabs.html"):
 #     animal = get_object_or_404(Animal, pk=animal_id)
