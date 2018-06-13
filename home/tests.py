74c6a1fdc1f9b8209cc01d147315fc2fc9cfb593
carlosribas <caduribas@gmail.com>
2018-06-13 18:07:17 -0300

    Fixing datepicker

--- home/tests.py
+++ home/tests.py
@@ -1,15 +1,36 @@
 # -*- coding: utf-8 -*-
+from django.contrib.auth.models import User
 from django.core.urlresolvers import resolve, reverse
 from django.test import TestCase
-from views import authentication
+from django.test.client import RequestFactory
+
+from views import home
+
+USER_USERNAME = 'user'
+USER_PWD = 'mypassword'
+USER_EMAIL = 'user@example.com'
 
 
-class AuthenticationTest(TestCase):
-    def test_authentication_view_status_code(self):
-        url = reverse('authentication')
+class HomeTest(TestCase):
+    def setUp(self):
+        """
+        Configure authentication and variables to start each test
+        """
+
+        self.user = User.objects.create_user(username=USER_USERNAME, email=USER_EMAIL, password=USER_PWD)
+        self.user.is_staff = True
+        self.user.save()
+
+        self.factory = RequestFactory()
+
+        logged = self.client.login(username=USER_USERNAME, password=USER_PWD)
+        self.assertEqual(logged, True)
+
+    def test_home_view_status_code(self):
+        url = reverse('home')
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
 
-    def test_authentication_url_resolves_authentication_view(self):
+    def test_home_url_resolves_home_view(self):
         view = resolve('/')
-        self.assertEquals(view.func, authentication)
+        self.assertEquals(view.func, home)
--- home/views.py
+++ home/views.py
@@ -1,30 +1,20 @@
 # -*- coding: utf-8 -*-
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.utils.translation import activate, LANGUAGE_SESSION_KEY, ugettext as _
+from django.utils.translation import activate, LANGUAGE_SESSION_KEY
 
 from client.models import Client
 from animal.models import Animal
 
 
 @login_required
-def home(request):
-    clients = Client.objects.all()
-    animals = Animal.objects.all()
+def home(request, template_name="home/home.html"):
+    total_of_clients = Client.objects.count()
+    total_of_animals = Animal.objects.count()
 
-    if request.method == 'POST':
-        animal_id = request.POST['animal']
-        if animal_id != '0' and animal_id != '':
-            redirect_url = reverse('animal_view', args=(animal_id,))
-            return HttpResponseRedirect(redirect_url)
-        else:
-            messages.warning(request, _("You should select an animal."))
-
-    context = {'clients': clients, 'animals': animals}
-    return render(request, 'home/home.html', context)
+    context = {'total_of_clients': total_of_clients, 'total_of_animals': total_of_animals}
+    return render(request, template_name, context)
 
 
 def language_change(request, language_code):
