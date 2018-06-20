8b141e915b769e2fbcf6743ae5d08968f15cf93e
Carlos Eduardo Ribas <caduribas@gmail.com>
2018-06-20 20:23:48 -0300

    Ops! I need to use jquery to filter breed and color when creating an animal

--- animal/static/animal/js/animal.js
+++ animal/static/animal/js/animal.js
@@ -2,7 +2,7 @@ $(function () {
     var $spay_neuter = $('#id_spay_neuter');
 
     $spay_neuter.each(function() {
-        if($(this).val() == 'y'){
+        if($(this).val() == 'yes'){
             $('#id_spay_neuter_date').prop('disabled', false);
         } else{
             $('#id_spay_neuter_date').prop('disabled', true);
@@ -10,10 +10,36 @@ $(function () {
     });
 
     $spay_neuter.on('change', (function () {
-       if($(this).val() == 'y'){
+       if($(this).val() == 'yes'){
            $('#id_spay_neuter_date').prop('disabled', false);
        } else {
            $('#id_spay_neuter_date').prop('disabled', true);
        }
     }));
 });
+
+function ajax_filter_specie_breed(specie_id) {
+    $("#id_breed").html('<option value="">Carregando...</option>');
+    $("#id_color").html('<option value="">Carregando...</option>');
+    $.ajax({
+        type: "GET",
+        url: "/animal/select_specie",
+        dataType: "json",
+        data: {'specie':specie_id},
+        success: function(retorno) {
+            $("#id_breed").empty();
+            $("#id_color").empty();
+            $("#id_breed").append('<option value="">--------</option>');
+            $("#id_color").append('<option value="">--------</option>');
+            $.each(retorno[0], function(i, item){
+                $("#id_breed").append('<option value="'+item.pk+'">'+item.valor+'</option>');
+            });
+            $.each(retorno[1], function(i, item){
+                $("#id_color").append('<option value="'+item.pk+'">'+item.valor+'</option>');
+            });
+        },
+        error: function(erro) {
+            alert('Erro: Sem retorno de requisição.');
+        }
+    });
+}
--- animal/views.py
+++ animal/views.py
@@ -1,13 +1,34 @@
+import json as simplejson
+
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.db.models.deletion import ProtectedError
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render, redirect
 from django.utils.translation import ugettext as _
 
-from models import Animal
+from models import Animal, Specie
 from forms import AddAnimalForm
+
+
+def select_specie(request):
+    if request.method == 'GET':
+        specie_id = request.GET.get('specie')
+        specie = get_object_or_404(Specie, id=specie_id)
+
+        breeds = specie.breed_set.all()
+        list_breed = []
+        for a in breeds:
+            list_breed.append({'pk': a.id, 'valor': a.__str__()})
+
+        colors = specie.color_set.all()
+        list_color = []
+        for a in colors:
+            list_color.append({'pk': a.id, 'valor': a.__str__()})
+
+        json = simplejson.dumps([list_breed, list_color])
+        return HttpResponse(json, content_type="application/json")
 
 
 @login_required
