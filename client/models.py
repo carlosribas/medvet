29974fd61e98353447784424a0e4069b10c05b34
Carlos Eduardo Ribas <caduribas@gmail.com>
2018-07-01 09:43:53 -0300

    Code optimization

--- client/models.py
+++ client/models.py
@@ -1,26 +1,14 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.core.exceptions import ValidationError
 from django_countries.fields import CountryField
-from validation import CPF
-
-
-def validate_cpf(value):
-    """
-    Checks if the CPF is valid
-
-    """
-    validation = CPF(value)
-    if not validation.isValid():
-        raise ValidationError(_('%s is not valid CPF') % value)
+from validation import validate_cpf
 
 
 class Client(models.Model):
     """
-    An instance of this class represents a client.
+    An instance of this class represents a customer.
 
-    '__unicode__'		Returns the name.
-    'class Meta'		Sets the description model (singular and plural) and define ordering of data by name.
+    '__str__'		Returns the name.
     """
     name = models.CharField(_('Name'), max_length=255)
     rg = models.CharField(_('RG'), max_length=12, blank=True)
@@ -38,28 +26,15 @@ class Client(models.Model):
     country = CountryField(_('Country'), blank=True, default='BR')
     note = models.CharField(_('Note'), max_length=255, blank=True)
 
-    # Returns the name 
-    def __unicode__(self):
-        return u'%s' % self.name
-
-    # Description of the model / Sort by name
-    class Meta:
-        verbose_name = _('Client')
-        verbose_name_plural = _('Clients')
-        ordering = ('name', )
+    def __str__(self):
+        return self.name
 
 
 class ClientContact(models.Model):
     """
-    An instance of this class represents a contact of client.
-
-    'class Meta'		Sets the description model (singular and plural) and define ordering of data by name.
+    An instance of this class represents a contact of a customer.
     """
     client_name = models.ForeignKey(Client)
     name = models.CharField(_('Name'), max_length=255, blank=True)
     phone = models.CharField(_("Phone"), max_length=15, blank=True)
     cellphone = models.CharField(_('Cell Phone'), max_length=15, blank=True)
-
-    class Meta:
-        verbose_name = _('Client contact')
-        verbose_name_plural = _('Client contacts')
--- client/validation.py
+++ client/validation.py
@@ -1,109 +1,48 @@
-# -*- coding:UTF-8 -*-
-# fonte: http://www.python.org.br/wiki/VerificadorDeCPF
-
 import re
 
-# traduz 123.456.789-10 para 12345678910
-_translate = lambda cpf: ''.join(re.findall("\d", cpf))
+from django.core.validators import EMPTY_VALUES
+from django.forms import ValidationError
+from django.utils.translation import ugettext_lazy as _
 
-def _exceptions(cpf):
-    """Se o número de CPF estiver dentro das exceções é inválido
+error_messages = {
+    'invalid': _("Invalid CPF number."),
+    'digits_only': _("This field requires only numbers."),
+    'max_digits': _("This field requires exactly 11 digits."),
+}
 
+
+def DV_maker(v):
+    if v >= 2:
+        return 11 - v
+    return 0
+
+
+def validate_cpf(value):
     """
-    if len(cpf)!=11:
-        return True
-    else:
-        s=''.join(str(x) for x in cpf)
-        if s=='00000000000' or s=='11111111111' or s=='22222222222' or s=='33333333333' or s=='44444444444' or s=='55555555555' or s=='66666666666' or s=='77777777777' or s=='88888888888' or s=='99999999999':
-            return True
-    return False
-
-def _gen(cpf):
-    """Gera o próximo dígito do número de CPF
-
+    Value can be either a string in the format XXX.XXX.XXX-XX or an
+    11-digit number.
     """
-    res = []
-    for i, a in enumerate(cpf):
-        b = len(cpf) + 1 - i
-        res.append(b * a)
 
-    res = sum(res) % 11
+    if value in EMPTY_VALUES:
+        return u''
+    if not value.isdigit():
+        value = re.sub("[-\.]", "", value)
+    orig_value = value[:]
+    try:
+        int(value)
+    except ValueError:
+        raise ValidationError(error_messages['digits_only'])
+    if len(value) != 11:
+        raise ValidationError(error_messages['max_digits'])
+    orig_dv = value[-2:]
 
-    if res > 1:
-        return 11 - res
-    else:
-        return 0
+    new_1dv = sum([i * int(value[idx]) for idx, i in enumerate(range(10, 1, -1))])
+    new_1dv = DV_maker(new_1dv % 11)
+    value = value[:-2] + str(new_1dv) + value[-1]
+    new_2dv = sum([i * int(value[idx]) for idx, i in enumerate(range(11, 1, -1))])
+    new_2dv = DV_maker(new_2dv % 11)
+    value = value[:-1] + str(new_2dv)
+    if value[-2:] != orig_dv:
+        raise ValidationError(error_messages['invalid'])
 
-
-class CPF(object):
-
-    _gen = staticmethod(_gen)
-    _translate = staticmethod(_translate)
-    
-    def __init__(self, cpf):
-        """O argumento cpf pode ser uma string nas formas:
-
-        12345678910
-        123.456.789-10
-
-        ou uma lista ou tuple
-        [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 0]
-        (1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 0)
-
-        """
-        
-        if isinstance(cpf, basestring):
-            if not cpf.isdigit():
-               cpf = self._translate(cpf)
-            
-        self.cpf = [int(x) for x in cpf]
-
-    def __getitem__(self, index):
-        """Retorna o dígito em home como string
-
-        """
-        
-        return self.cpf[index]
-
-    def __repr__(self):
-        """Retorna uma representação 'real', ou seja:
-
-        eval(repr(cpf)) == cpf
-        
-        """
-        
-        return "CPF('%s')" % ''.join(str(x) for x in self.cpf)
-
-    def __eq__(self, other):
-        """Provê teste de igualdade para números de CPF
-
-        """
-
-        return isinstance(other, CPF) and self.cpf == other.cpf
-    
-    def __str__(self):
-        """Retorna uma representação do CPF na forma:
-
-        123.456.789-10
-
-        """
-
-        d = iter("..-")
-        s = map(str, self.cpf)
-        for i in xrange(3, 12, 4):
-            s.insert(i, d.next())
-        r = ''.join(s)
-        return r
-
-    def isValid(self):
-        """Valida o número de cpf
-
-        """
-        
-        if _exceptions(self.cpf):
-            return False
-
-        s = self.cpf[:9]
-        s.append(self._gen(s))
-        s.append(self._gen(s))
-        return s == self.cpf[:]
+    return orig_value
