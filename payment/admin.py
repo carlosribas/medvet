24506116d9e5a82b68a7a0fe223e35ef70a6a47d
carlosribas <caduribas@gmail.com>
2018-07-05 14:46:46 -0300

    Removing unused app

--- payment/admin.py
+++
@@ -1,9 +0,0 @@
-# from django.contrib import admin
-#
-# from services.models import *
-#
-# # admin.site.register(ConsultationType)
-# admin.site.register(SurgicalProcedure)
-# admin.site.register(VaccineType)
-# admin.site.register(ExamType)
-# admin.site.register(AmbulatoryService)
--- payment/urls.py
+++
@@ -1,7 +0,0 @@
-# from django.conf.urls import url
-#
-# from payment import views
-#
-# urlpatterns = [
-#     url(r'^list', views.unpaid_services_list, name='unpaid_services_list'),
-# ]
