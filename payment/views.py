f87bdbb49173cddab88c72725ac8e9b049901579
carlosribas <caduribas@gmail.com>
2018-09-18 19:12:53 -0300

    Using django-filter app to filter unpaid service

--- payment/views.py
+++ payment/views.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import datetime
 
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -11,60 +10,15 @@ from django.utils.translation import ugettext_lazy as _
 
 from payment.forms import PaymentForm
 from services.models import Service, Consultation, Vaccine, ExamName, CONSULTATION, VACCINE, EXAM
-from client.models import Client
+from services.filters import ServiceFilter
 
 
 @login_required
 def unpaid(request, template_name="payment/unpaid.html"):
     unpaid_list = Service.objects.filter(paid='no').order_by('date')
-    customers = Client.objects.filter(id__in=unpaid_list.values_list('animal__owner').distinct())
+    unpaid_list = ServiceFilter(request.GET, queryset=unpaid_list)
 
-    if request.method == "POST":
-        if request.POST['action'] == "search":
-            try:
-                start_date = datetime.datetime.strptime(request.POST['start_date'], "%d/%m/%Y").date()
-            except ValueError:
-                start_date = False
-
-            try:
-                end_date = datetime.datetime.strptime(request.POST['end_date'], "%d/%m/%Y").date()
-            except ValueError:
-                end_date = False
-
-            if start_date and not end_date or not start_date and end_date:
-                messages.error(request, _('You must select start date and end date.'))
-
-            elif end_date < start_date:
-                messages.error(request, _('The end date must be greater than the start date.'))
-
-            elif not start_date and not end_date and request.POST['customer'] == '':
-                messages.error(request, _('You must select start date and end date or select a customer.'))
-
-            elif start_date and end_date and request.POST['customer'] != '':
-                unpaid_list = Service.objects.filter(
-                    paid='no',
-                    date__gte=start_date,
-                    date__lte=end_date,
-                    animal__owner=request.POST['customer']
-                ).order_by('date')
-
-            elif start_date and end_date and request.POST['customer'] == '':
-                unpaid_list = Service.objects.filter(
-                    paid='no',
-                    date__gte=start_date,
-                    date__lte=end_date,
-                ).order_by('date')
-
-            elif not start_date and not end_date and request.POST['customer'] != '':
-                unpaid_list = Service.objects.filter(
-                    paid='no',
-                    animal__owner=request.POST['customer']
-                )
-
-            else:
-                messages.error(request, _('There is something wrong here!'))
-
-    context = {"unpaid_list": unpaid_list, "customers": customers}
+    context = {"unpaid_list": unpaid_list}
     return render(request, template_name, context)
 
 
