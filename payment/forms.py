1a0d1308938912eeadbf5cce3146a60783f12571
Carlos Eduardo Ribas <caduribas@gmail.com>
2018-09-18 23:05:04 -0300

    Bug fix

--- payment/forms.py
+++ payment/forms.py
@@ -6,14 +6,18 @@ from payment.models import Payment
 
 
 class PaymentForm(forms.ModelForm):
+    date = forms.DateField(
+        input_formats=['%d/%m/%Y'], required=True, widget=forms.DateInput(
+            attrs={'class': 'form-control datepicker', 'data-error': _('This field must be filled.')},
+            format='%d/%m/%Y')
+    )
+
     class Meta:
         model = Payment
         exclude = ['service', 'total']
 
         widgets = {
             'payment_method': Select(attrs={'class': 'form-control'}),
-            'date': DateInput(attrs={'class': 'form-control datepicker', 'required': "",
-                                     'data-error': _('This field must be filled.')}, ),
             'discount_or_increase': NumberInput(attrs={'class': 'form-control', 'onchange': 'updateInput()'}),
             'note': Textarea(attrs={'class': 'form-control', 'rows': '4'}),
         }
