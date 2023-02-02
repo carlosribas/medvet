6d85ec9b13636b3c51da7f7bf2a3d95d48dd9393
Jorgelig <jorgeligg@gmail.com>
2023-02-01 23:25:18 -0600

    feat: add es_MX locale

--- medvet/settings.py
+++ medvet/settings.py
@@ -108,7 +108,7 @@ LANGUAGE_CODE = 'en'
 LANGUAGES = (
     ('pt-br', _('Portuguese')),
     ('en', _('English')),
-    ('es-MX', _('Español')),
+    ('es-mx', _('Español')),
 )
 
 LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
